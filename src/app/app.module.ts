3006cf910b907aad6e0e38fb03699c49a03250b9
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-23 23:26:50 +0200

    Added Imprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
+import { ImprintComponent } from './components/imprint/imprint.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +30,9 @@ import { MatMenuModule } from "@angular/material/menu";
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
